ff90add9ae136eb758c7a6ebf1371cda76874265
hanna <hanna_a@ukr.net>
2019-08-25 12:22:29 +0300

    Initial commit

---
+++ src/main/java/task1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class task1 {
+    public static void main(String[] args)  {
+
+            Scanner in = new Scanner(System.in);
+            System.out.println("Enter first number and press button Enter");
+            int num1=in.nextInt();
+
+            System.out.println("Enter second number and press button Enter");
+            int num2=in.nextInt();
+
+            System.out.println("Here is sum of your numbers:");
+            System.out.println(num1+num2);
+
+        }
+    }
