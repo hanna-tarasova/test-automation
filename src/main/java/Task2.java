dd0cd29038e6248b1c24fb771899a3333052307a
hanna <hanna_a@ukr.net>
2019-08-29 01:20:52 +0300

    fixes

--- src/main/java/Task2.java
+++ src/main/java/Task2.java
@@ -24,42 +24,43 @@ public class Task2 {
         // объявляем некий новый массив, пока пустой, но содержащий ровно столько элемнтов, сколько наш массив 2.
         // в новый массив мы будем вносить сначала числа из первого массива, которые выполняют условие - являются парными
         int[] array3 = new int[10];
+        int[] array4 = new int[10];
         // показываем иизначальный массив
         System.out.println("Here is a second array: " + Arrays.toString(array2));
 
-
+        //берём вспомогательные переменные - первая ячейка будущего массива, и последняя
         int count1 = 0;
         int count2 = 9;
+        // перебираем элементы изначального массива
         for (int i = 0; i < array2.length; i++)
         {
+            // если остаток от деления равен нулю, то это значение занимает первую ячейку
             if ((array2[i] % 2) == 0)
             {
                 array3[count1] = array2[i];
                 count1++;
             }
+            // если остаток от деления равен единице (условие - иначе) то занимает последнюю ячейку
             else
             {
-            array3[count2] = array2[i];
-            count2--;
+                array3[count2] = array2[i];
+                count2--;
             }
 
         }
-      System.out.println("Here are even numbers, then odd numbers of second array: " + Arrays.toString(array3));
+        //берём ещё раз вспомогательные переменные, чтобы отсортировать нечётные числа в порядок, указанный вначале
+        int count3 = 0;
+        int count4 = 9;
+        for (int j = 0; j < array4.length; j++) {
 
+            if ((array3[j] % 2) == 0) {
+                array4[count3] = array3[j];
+                count3++;
+            } else {
+                array4[count4] = array3[j];
+                count4--;
+            }
+        }
+        System.out.println("Here are even numbers, then odd numbers of second array: " + Arrays.toString(array4));
     }
 }
-
-
-
-
-
-
-
-
-        //
-
-
-
-
-
-
