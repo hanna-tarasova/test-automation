ca949a7e28651ccddfb171d21b674955fd920d44
hanna <hanna_a@ukr.net>
2019-09-19 00:45:03 +0300

    HomeWork done

---
+++ src/test/java/Selenium/Driver.java
@@ -0,0 +1,70 @@
+package Selenium;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Driver {
+
+
+    private static String arguments;
+
+    public enum Browser {
+        CHROME,
+        FIREFOX,
+        EDGE
+    }
+
+
+
+
+
+
+    public static WebDriver getDriver (Browser browser, String arguments) {
+        WebDriver driver;
+
+        {
+
+            switch (browser) {
+                case CHROME:
+                    WebDriverManager.chromedriver ().setup ();
+                    ChromeOptions options = new ChromeOptions();
+                    options.addArguments("start-fullscreen");
+                    WebDriver driverChrome = new ChromeDriver (options);
+                    return driverChrome;
+
+               case FIREFOX:
+                    WebDriverManager.firefoxdriver ().setup ();
+                    WebDriver driverFirefox = new FirefoxDriver ();
+                    driverFirefox.manage().window().maximize();
+                    return driverFirefox;
+
+
+               case EDGE:
+                    WebDriverManager.edgedriver ().setup ();
+                    WebDriver driverEdge = new EdgeDriver ();
+                    return driverEdge;
+
+
+
+            }
+            ;
+        }
+        return null;
+    };
+    public static void main (String[] args) {
+        String baseURL = "https://www.ukr.net/ua/";
+
+        for (Browser browser : Browser.values ()) {
+            System.out.println (browser.toString ());
+            WebDriver driver = Driver.getDriver (browser, "full-screen");
+            driver.get (baseURL);
+            driver.quit ();
+        }
+    }
+
+
+};
